55c4749631a994b115cb15f16d828cdceca0bdd8
HilmiAliSalsabila <alihilmi93@gmail.com>
2025-12-22 13:33:41 +0700

    new setup folder and gitignore

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,7 +1,7 @@
+export default config;
+/** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    '@tailwindcss/postcss': {},
   },
-};
-
-export default config;
+}
