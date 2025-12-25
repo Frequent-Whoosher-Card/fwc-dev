dc2669b66e4980a162c601702f7c4e66fcdd7b4f
rdsarjito <rama.dhaninursarjito@gmail.com>
2025-12-25 14:00:43 +0700

    chore: update port configuration

--- frontend/lib/apiConfig.ts
+++ frontend/lib/apiConfig.ts
@@ -1,7 +1,7 @@
 const fallbackBaseUrl =
   process.env.NODE_ENV === "production"
     ? undefined
-    : "http://localhost:3000";
+    : "http://localhost:3001";
 
 export const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL || fallbackBaseUrl || "";
