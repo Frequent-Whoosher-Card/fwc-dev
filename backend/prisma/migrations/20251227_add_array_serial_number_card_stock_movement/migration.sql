431d3d97304e9dd421d1d1d94c0a2053e449e37c
Iqbal Ramadan <balramadan@hotmail.com>
2025-12-27 17:54:48 +0700

    feat: Add `lost_serial_numbers` array column to `card_stock_movements` and migrate `CardStatus` enum values with old string mappings.

--- backend/prisma/migrations/20251227_add_array_serial_number_card_stock_movement/migration.sql
+++ backend/prisma/migrations/20251227_add_array_serial_number_card_stock_movement/migration.sql
@@ -1,13 +1,19 @@
 -- AlterEnum
-BEGIN;
+
 CREATE TYPE "CardStatus_new" AS ENUM ('IN_OFFICE', 'IN_TRANSIT', 'IN_STATION', 'LOST', 'SOLD_ACTIVE', 'SOLD_INACTIVE');
 ALTER TABLE "public"."cards" ALTER COLUMN "status_card" DROP DEFAULT;
-ALTER TABLE "cards" ALTER COLUMN "status_card" TYPE "CardStatus_new" USING ("status_card"::text::"CardStatus_new");
+ALTER TABLE "cards" ALTER COLUMN "status_card" TYPE "CardStatus_new" USING (
+  CASE "status_card"::text
+    WHEN 'Non_Aktif' THEN 'SOLD_INACTIVE'::"CardStatus_new"
+    WHEN 'Aktif' THEN 'SOLD_ACTIVE'::"CardStatus_new"
+    ELSE "status_card"::text::"CardStatus_new"
+  END
+);
 ALTER TYPE "CardStatus" RENAME TO "CardStatus_old";
 ALTER TYPE "CardStatus_new" RENAME TO "CardStatus";
 DROP TYPE "public"."CardStatus_old";
 ALTER TABLE "cards" ALTER COLUMN "status_card" SET DEFAULT 'IN_OFFICE';
-COMMIT;
+
 
 -- AlterTable
 ALTER TABLE "card_stock_movements" ADD COLUMN     "lost_serial_numbers" TEXT[] DEFAULT ARRAY[]::TEXT[],
